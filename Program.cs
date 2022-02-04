80a430d9edeb926c9c665a0baea2e28f732e34ea
MatheusVarella <varellamat@gmail.com>
2022-02-04 15:39:05 -0300

    Iniciando projeto criando a conexão

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CursoDapper.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace CursoDapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Passowrd=#12345678";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+            }
+        }
+    }
+}
